f0849eb39e072a5a299c0912b3b7cc5df04941f9
Carlos Encine <carlosencinedotcom@gmail.com>
2020-11-17 22:18:54 -0300

    Update controleBonificacao.java
    aqui observei algumas coisas, como o uso da classe em outros pontos do projeto

--- bytebank-herdado/src/controleBonificacao.java
+++ bytebank-herdado/src/controleBonificacao.java
@@ -1,6 +1,8 @@
 
 public class controleBonificacao {
-	
+	// classe esta ok para inicio, mas da para fazer melhor
+	// mas não vou cobrar a melhoria agora, pois lá na frente tu vai perceber essa necessidade de melhoria e saber como fazer
+	// só vou fazer um adendo para que essa classe seja usada nas demais implementações
 	private double soma;
 	
 	//metodo
