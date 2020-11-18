e7f353b0a6113d7f7124a9e2faef8c7f28b92d36
Carlos Encine <carlosencinedotcom@gmail.com>
2020-11-17 22:20:03 -0300

    Update programador.java

--- bytebank-herdado/src/programador.java
+++ bytebank-herdado/src/programador.java
@@ -1,7 +1,8 @@
 
 
 public class programador extends Funcionario {
-	
+	// poderia ter colocado caracteristicas ou metodos specificos do programador
+	// novamente poderia ter usado a classe existente para tratar o calculo da bonificação
 	public double getBonificacao() {
 		System.out.println("chamando o metodo de bonificacao do programador.");
 		return super.getBonificacao() + 100;
