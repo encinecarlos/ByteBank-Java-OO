c52d82c8b64d46a1ca91c0e9ce285e04d0f740c3
Carlos Encine <carlosencinedotcom@gmail.com>
2020-11-17 22:13:32 -0300

    Update Funcionario.java
    code review da classe funcionario com pequenas revisões

--- bytebank-herdado/src/Funcionario.java
+++ bytebank-herdado/src/Funcionario.java
@@ -1,15 +1,20 @@
 //super class
 public abstract class  Funcionario { // uma classe que serve de modelo para as sub class
 	//atributos da classe (objeto)
+	
+	//Um funcionário em essencia é uma pessoa, logo a classe generica correta seria Pessoa e Funcionário herda de Pessoa
+	// pois uma pessoa é um funcionario, em resumo cabe criar essa classe pessoa e retirar os atributos nome e cpf daqui
 	private String nome;
 	private String cpf;
 	protected double salario;
 	
 	//metodo get & set
+	// O metodo bonificação não tem sentido estar aqui poderia ficar em uma classe propria
 	public double getBonificacao() {
 		return this.salario * 0.05;
 	}
 	
+	//getters e setters de nome e cpf podem ser retirados daqui seguindo a orientação no inicio do arquivo
 	public String getNome() {
 		return nome;
 	}
