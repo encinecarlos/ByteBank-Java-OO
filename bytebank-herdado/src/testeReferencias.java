dbc8e284ed3d2ec987f78537f0e1c803a98a0b0f
Carlos Encine <carlosencinedotcom@gmail.com>
2020-11-17 22:26:15 -0300

    Update testeReferencias.java

--- bytebank-herdado/src/testeReferencias.java
+++ bytebank-herdado/src/testeReferencias.java
@@ -1,9 +1,12 @@
 
 public class testeReferencias {
-	
+	// poderia ter criado um arquivo chamado main.java e feito todos os testes dentro dele
+	// pois via de regra uma aplicação tem um e somente um ponto de entrada, então vários aquiuvos com a declaração main acabam mais por atrapalhar do que ajudar
 	public static void main(String[] args) {
+	// O projeto como um todo esta legal para o teu estagio atual João, só carece de pequenas melhorias mesmo, nada muito complicado, algumas tu vai perceber mais a frente
+	// quando estiver mais avançado no aprendizado, e mais um detalhe, procura manter atenção na identação, pois em algumas classes percebi algumas pequenas quebras na mesma ok
+	// No mais ta de parabéns ah mais uma coisa procura fazer uso dos construtorres, pois para certas coisas eles podem ser beeeeeem uteis
 		
-	
 	Gerente gerente1 = new Gerente();
 	
 	gerente1.setNome("Joao C");
